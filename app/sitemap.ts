387d9846f3eecdc38a0a5c5f166e12d6b71fe2fe
Binho35 <128901049+Binho35@users.noreply.github.com>
2026-09-05 09:44:09 -0300

    feat: inclui política de privacidade no sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,5 +8,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1,
     },
+    {
+      url: "https://bravsystems.com.br/privacidade",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.4,
+    },
   ];
 }
